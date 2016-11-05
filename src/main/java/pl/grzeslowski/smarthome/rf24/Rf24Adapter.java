09959c57546813d7165c93ea967b46eb94172c99
martin <martin.grzeslowski@gmail.com>
2016-11-05 22:50:57 +0100

    Fixing compilaton errors

--- src/main/java/pl/grzeslowski/smarthome/rf24/Rf24Adapter.java
+++ src/main/java/pl/grzeslowski/smarthome/rf24/Rf24Adapter.java
@@ -1,9 +1,10 @@
-package pl.grzeslowski.smarthome;
+package pl.grzeslowski.smarthome.rf24;
 
-import pl.grzeslowski.smarthome.generated.*;
+
+import pl.grzeslowski.smarthome.rf24.generated.RF24;
 
 public class Rf24Adapter {
-    private pl.grzeslowski.smarthome.generated.RF24 rf24;
+    private RF24 rf24;
 
     public Rf24Adapter() {
         rf24 = new RF24((short)1, (short)2);
