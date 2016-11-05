1094bbfd05ef50c395172ccda0ce20787a92f2d9
martin <martin.grzeslowski@gmail.com>
2016-11-06 00:36:23 +0100

    Compiling error

--- src/main/java/pl/grzeslowski/smarthome/rf24/Rf24Adapter.java
+++ src/main/java/pl/grzeslowski/smarthome/rf24/Rf24Adapter.java
@@ -7,6 +7,6 @@ public class Rf24Adapter {
     private RF24 rf24;
 
     public Rf24Adapter() {
-        rf24 = new RF24((short)1, (short)2);
+//        rf24 = new RF24((short)1, (short)2);
     }
 }
