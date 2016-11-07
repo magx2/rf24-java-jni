ca7f4fb163b2854f8b12392337d55aaeb44208fb
martin <martin.grzeslowski@gmail.com>
2016-11-07 22:48:25 +0100

    Removing build examples

--- native/install.sh
+++ native/install.sh
@@ -26,4 +26,3 @@ sudo make install -B -C ${ROOT_PATH}/RF24Mesh
 echo "Installing RF24Gateway Repo..."
 git clone https://github.com/tmrh20/RF24Gateway.git ${ROOT_PATH}/RF24Gateway
 sudo make install -B -C ${ROOT_PATH}/RF24Gateway
-make -B -C${ROOT_PATH}/RF24Gateway/examples/ncurses; echo ""; echo "Complete, to run the example, cd to rf24libs/RF24Gateway/examples/ncurses and enter  sudo ./RF24Gateway_ncurses";;
