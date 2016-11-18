38a9c29368bba0b687235691a935b1df3eb747e6
martin <martin.grzeslowski@gmail.com>
2016-11-18 18:38:11 +0100

    Adding retry to options

--- examples/src/main/java/pl/grzeslowski/smarthome/rf24/examples/cmd_line/ArgsReader.java
+++ examples/src/main/java/pl/grzeslowski/smarthome/rf24/examples/cmd_line/ArgsReader.java
@@ -11,8 +11,8 @@ public class ArgsReader {
     private static final String CMD_CE = "ce";
     private static final String CMD_CSN = "csn";
     private static final String CMD_CLOCK_SPEED = "clock_speed";
-    private static final String RETRY_DELAY = "retry_delay";
-    private static final String RETRY_NUMBER = "retry_number";
+    private static final String CMD_RETRY_DELAY = "retry_delay";
+    private static final String CMD_RETRY_NUMBER = "retry_number";
     private static final String CMD_NUMBER_OF_SENDS = "number_of_sends";
 
     private static final short DEFAULT_CE = 22;
@@ -29,6 +29,10 @@ public class ArgsReader {
         options.addOption(CMD_CE, true, "CE pin");
         options.addOption(CMD_CSN, true, "CSN pin");
         options.addOption(CMD_CLOCK_SPEED, true, "Clock speed pin");
+        options.addOption(CMD_RETRY_DELAY, true, format("How long should wait before sending a retry message. Defaulr %s.",
+                DEFAULT_RETRY_DELAY));
+        options.addOption(CMD_RETRY_NUMBER, true, format("How many times should retry sending message. Default %s.",
+                DEFAULT_RETRY_NUMBER));
         options.addOption(CMD_NUMBER_OF_SENDS, true, format("How much messages server should send. Default %s.",
                 DEFAULT_NUMBER_OF_SENDS));
     }
@@ -42,8 +46,8 @@ public class ArgsReader {
     }
 
     public Retry readRetry(String[] args) {
-        short retryDelay = read(args, RETRY_DELAY, DEFAULT_RETRY_DELAY);
-        short retryNumber = read(args, RETRY_NUMBER, DEFAULT_RETRY_NUMBER);
+        short retryDelay = read(args, CMD_RETRY_DELAY, DEFAULT_RETRY_DELAY);
+        short retryNumber = read(args, CMD_RETRY_NUMBER, DEFAULT_RETRY_NUMBER);
 
         return new Retry(retryDelay, retryNumber);
     }
